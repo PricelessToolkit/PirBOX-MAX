ce048debd5cf3e8aaf31edaa569bb2a006474cbb
PricelessToolkit <81021972+PricelessToolkit@users.noreply.github.com>
2025-07-10 17:30:43 +0200

    Update config.h

--- code/PirBox-Max/config.h
+++ code/PirBox-Max/config.h
@@ -29,15 +29,21 @@
 // SF9: Approximately 0.100 seconds (100.147 milliseconds)
 // SF10: Approximately 0.193 seconds (193.413 milliseconds)
 // SF11: Approximately 0.385 seconds (385.297 milliseconds)
-// SF12: Approximately 0.746 seconds (746.127 milliseconds)
 //
 // These calculations demonstrate how the time on air increases with higher Spreading Factors
 // due to the decreased data rate, which is a trade-off for increased communication range and signal robustness.
 
 #define BAND                      868E6     // 433E6 MHz or 868E6 MHz or 915E6 MHz
 #define TX_OUTPUT_POWER           20        // dBm tx output power
-#define LORA_BANDWIDTH            4         // bandwidth 2: 31.25Khz, 3: 62.5Khz, 4: 125Khz, 5: 250KHZ, 6: 500Khz
-#define LORA_SPREADING_FACTOR     8         // spreading factor 6-12 [SF5..SF12]
+
+//| Bandwidth | Supported Spreading Factors |
+//|-----------|-----------------------------|
+//| 125 kHz   | SF5 – SF9                   |
+//| 250 kHz   | SF5 – SF10                  |
+//| 500 kHz   | SF5 – SF11                  |
+
+#define LORA_BANDWIDTH            4         // bandwidth 4: 125Khz, 5: 250KHZ, 6: 500Khz
+#define LORA_SPREADING_FACTOR     8         // spreading factor 5-11 [SF5..SF11]
 #define LORA_CODINGRATE           1         // [1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8]
 #define LORA_PREAMBLE_LENGTH      6         // Same for Tx and Rx
 #define LORA_PAYLOADLENGTH        0         // 0: Variable length packet (explicit header),  1..255 for Fixed length packet (implicit header)
